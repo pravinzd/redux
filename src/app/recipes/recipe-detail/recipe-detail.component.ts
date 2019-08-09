d2385fffbe2d9a2aa859c02a836f2cd7a2b12c12
pravinzd <pravin.zd@gmail.com>
2019-08-09 16:46:12 +0530

    Course Project - Routing

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -10,22 +10,22 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
- recipe: Recipe;
- id: number; 
+  recipe: Recipe;
+  id: number;
 
   constructor(private recipeService: RecipeService,
     private route: ActivatedRoute,
     private router: Router) {
-     }
+  }
 
   ngOnInit() {
-   this.route.params
-   .subscribe(
-     (params: Params) => {
-       this.id = +params['id']; 
-       this.recipe = this.recipeService.getRecipe(this.id);
-     }
-   );
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.recipe = this.recipeService.getRecipe(this.id);
+        }
+      );
   }
 
   onAddToShoppingList() {
@@ -33,7 +33,7 @@ export class RecipeDetailComponent implements OnInit {
   }
 
   onEditRecipe() {
-    this.router.navigate(['edit'], {relativeTo: this.route});
+    this.router.navigate(['edit'], { relativeTo: this.route });
     // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route});
   }
 
